2b8b315f1ac11b90810f544751f971d7a93e5d37
daniloweber <weberd.wwi22@student.dhbw-heidenheim.de>
2023-04-11 11:58:05 +0200

    read content from json file

--- app/page.js
+++ app/page.js
@@ -1,10 +1,25 @@
 import Link from "next/link";
+import fsPromises from 'fs/promises';
+import path from 'path'
+import {log} from "next/dist/server/typescript/utils";
+export async function getStaticPropsRoot() {
+    const filePath = path.join(process.cwd(), '/public/content/root/content1.json');
+    const jsonData = await fsPromises.readFile(filePath);
+    const objectData = JSON.parse(jsonData);
 
-export default function Home() {
+    return {
+        props: objectData
+    }
+}
+
+export default async function Home() {
+  const data = await getStaticPropsRoot();
   return (
       <div>
         <div>
-            <h1>Homepage</h1>
+            <h1>{data.props.title}</h1>
+            <br/>
+            <p>{data.props.content}</p>
         </div>
         
     </div>
