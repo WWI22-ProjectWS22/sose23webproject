b1c0e50a25b3359213b9c73b7e4797c5ec10d720
daniloweber <weberd.wwi22@student.dhbw-heidenheim.de>
2023-04-14 13:45:39 +0200

    finished header

--- components/Header.js
+++ components/Header.js
@@ -3,35 +3,46 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-            <header>
-                <nav className={"navbar navbar-expand-md"}>
-                    <div className={"collapse navbar-collapse justify-content-center text-center"}
-                         id={"navbarNavAltMarkup"}>
-                        <ul className={"navbar-nav"}>
-                            <li className={"nav-item"}>
-                                <Link className={"nav-link"} href={"/"}>Home</Link>
-                            </li>
-                            <li className={"nav-item"}>
-                                <Link className={"nav-link"} href={"/Produkte"}>Produkte</Link>
-                            </li>
-                            <li className={"nav-item"}>
-                                <Link className={"nav-link"} href={"/Herstellung"}>Herstellung</Link>
-                            </li>
-                            <li className={"nav-item"}>
-                                <Link className={"nav-link"} href={"/Geschenkideen"}>Geschenkideen</Link>
-                            </li>
-                            <li className={"nav-item"}>
-                                <Link className={"nav-link"} href={"/Uberuns"}>Über Uns</Link>
-                            </li>
-                            <li className={"nav-item"}>
-                                <Link className={"nav-link"} href={"/Kontakt"}>Kontakt</Link>
-                            </li>
-                            <li className={"nav-item"}>
-                                <Link className={"nav-link"} href={"/TestPage"}>TestPage</Link>
-                            </li>
-                        </ul>
+            <>
+                <div className="container pt-3">
+                    <div className="row row-cols-2 row-cols-md-3">
+                        <div className="col-md-1">
+                        <img className="img-fluid rounded" src="/images/root/RCDlogoWT.webp" alt="Logo"/>
+                        
+                        
+                        </div>
+                        <div className="col">
+                        <h3>Royale Chocolate <br/> Design</h3>
+                        </div>
+                        <div className="col d-none d-md-inline"></div>
                     </div>
-                </nav>
-            </header>
+                </div>
+
+                <div className="container py-3 sticky-md-top">
+                    <nav className="navbar navbar-expand navbar-light bg-light rounded-pill">
+                        <div className="container-fluid">
+                            <ul className="navbar-nav justify-content-evenly w-100">
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/Produkte">Produkte</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/Herstellung">Herstellung</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/Überuns">Über uns</a>
+                            </li>
+                            <li className="nav-item active">
+                                <a className="nav-link" href="/Kontakt">Kontakt</a>
+                            </li>
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </>
+
+        
     )
 }
